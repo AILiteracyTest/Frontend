1c045a6c80eda257d44569186f02f20b9e4baf7b
gracelee5 <113881477+gracelee5@users.noreply.github.com>
2025-10-14 17:12:06 +0900

    Design: 결과 페이지 디자인

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -1,7 +1,25 @@
+import WhiteCard from "../components/WhiteCard";
+import GrayCard from "../components/GrayCard";
+import Button from "../components/Button";
 export default function ResultPage() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">결과 페이지</h1>
+    <div className="bg-app-bg flex items-center justify-center min-h-screen p-4 w-screen">
+      <WhiteCard>
+        <h1 className="text-3xl font-bold mb-4 mt-4">테스트 결과</h1>
+        <GrayCard className="text-xl font-bold ">
+          10개 중 7개를 맞추셨습니다
+        </GrayCard>
+        <div className="flex gap-8 mb-6 mt-8">
+          <GrayCard className=" h-64 w-1/2 rounded-md flex items-center justify-center">
+            {" "}
+            그래프
+          </GrayCard>
+          <GrayCard className=" h-64 w-1/2 rounded-md flex items-center justify-center">
+            n명 중 n위
+          </GrayCard>
+        </div>
+        <Button>친구에게 공유하기</Button>
+      </WhiteCard>
     </div>
   );
 }
